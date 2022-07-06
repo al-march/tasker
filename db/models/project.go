d8d672140bb605e071540d03ff687b607b0186c5
Alexander <valverin95@gmail.com>
2022-07-06 22:38:12 +0300

    🔥️ [BE] Models.Project: added time fields

--- db/models/project.go
+++ db/models/project.go
@@ -1,6 +1,9 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Project struct {
 	gorm.Model
@@ -10,4 +13,26 @@ type Project struct {
 
 	Tasks []Task `gorm:"foreignKey:ProjectID"`
 	Tags  []Tag  `gorm:"many2many:project_tags"`
+}
+
+func (p Project) Dto() ProjectDto {
+	return ProjectDto{
+		ID:          p.ID,
+		UserID:      p.UserID,
+		Title:       p.Title,
+		Description: p.Description,
+		Tags:        p.Tags,
+		CreatedAt:   p.CreatedAt,
+		UpdatedAt:   p.UpdatedAt,
+	}
+}
+
+type ProjectDto struct {
+	ID          uint      `json:"id"`
+	UserID      uint      `json:"userId"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Tags        []Tag     `json:"tags"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
