863368deaffd286ec1698cdc05295c5cc14846fb
Alexander <valverin95@gmail.com>
2022-07-11 00:22:55 +0300

    🔥️ [FE] Added links to projects

--- client/src/app/pages/main/main.component.ts
+++ client/src/app/pages/main/main.component.ts
@@ -1,22 +1,30 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ProjectService } from '@app/core/services';
+import { ProjectDto } from '@app/core/dto';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    'class': 'flex-1'
+  }
 })
 export class MainComponent implements OnInit {
 
+  projects: ProjectDto[] = [];
+
   constructor(
-    private projects: ProjectService
+    private projectService: ProjectService,
+    private ref: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
-    this.projects.getAll().subscribe(projects => {
-      console.log(projects);
-    })
+    this.projectService.getAll().subscribe(projects => {
+      this.projects = projects;
+      this.ref.markForCheck();
+    });
   }
 
 }
