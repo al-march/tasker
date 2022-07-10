adaa0c35647e4e45a08394b9fe26388893e96cf3
Alexander <valverin95@gmail.com>
2022-07-10 20:34:36 +0300

    🔥️ [FE] Services.Tag

---
+++ client/src/app/core/services/tag/tag.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { BaseApiService } from '@app/core/services';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService extends BaseApiService {
+}
