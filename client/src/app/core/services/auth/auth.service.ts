60ef7aefa3a717d7c0f15445131a1e4b0dfcebe4
Alexander <valverin95@gmail.com>
2022-07-10 20:21:48 +0300

    ✏ [FE] Services.Auth

--- client/src/app/core/services/auth/auth.service.ts
+++ client/src/app/core/services/auth/auth.service.ts
@@ -1,51 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Config } from '@app/core/services';
-
-export interface LoginDto {
-  login: string,
-  password: string,
-}
-
-export interface RegistrationDto {
-  login: string,
-  password: string,
-  name: string
-}
-
-export interface LoginResponse {
-  token: string;
-  user: UserDto;
-}
-
-export interface UserDto {
-  id: number,
-  login: string,
-  name?: string,
-  surname?: string,
-  email?: string,
-  phone?: string
-}
+import { BaseApiService } from '@app/core/services';
+import { LoginDto, LoginResponseDto, RegistrationDto, UserDto } from '@app/core/dto';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  get url() {
-    return this.config.host;
-  }
-
-  constructor(
-    private config: Config,
-    private http: HttpClient
-  ) {
-  }
+export class AuthService extends BaseApiService {
 
   registration(dto: RegistrationDto) {
     return this.http.post<UserDto>(`${this.url}/auth/registration`, dto);
   }
 
   login(dto: LoginDto) {
-    return this.http.post<LoginResponse>(`${this.url}/auth/login`, dto);
+    return this.http.post<LoginResponseDto>(`${this.url}/auth/login`, dto);
   }
 }
