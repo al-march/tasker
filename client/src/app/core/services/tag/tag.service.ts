2a31898b64596ac3d32ae6c770b24350b0853f7d
daraivanova <kudiyarova.1975@gmail.com>
2022-07-11 13:02:20 +0300

    ⚙️ [FE] Созданны методы для тэгов

--- client/src/app/core/services/tag/tag.service.ts
+++ client/src/app/core/services/tag/tag.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ApiRoute, BaseApiService } from '@app/core/services';
+import { TagColorDto, TagCreateDto, TagDto } from '@app/core/dto';
 
 @ApiRoute('tag')
 @Injectable({
   providedIn: 'root'
 })
 export class TagService extends BaseApiService {
+
+  create(dto: TagCreateDto) {
+    return this.http.post<TagDto>(`${this.url}`, dto);
+  }
+
+  update(dto: TagDto) {
+    return this.http.put<TagDto>(`${this.url}/${dto.id}`, dto);
+  }
+
+  delete(id: number) {
+    return this.http.delete<TagDto>(`${this.url}/${id}`);
+  }
+
+  getAll() {
+    return this.http.get<TagDto[]>(`${this.url}/all`);
+  }
+
+  getColor() {
+    return this.http.get<TagColorDto[]>(`${this.url}/colors`);
+  }
 }
