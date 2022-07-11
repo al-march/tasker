c2510279d96315b468d8985b7ba9bcfeebdd73fd
Alexander <valverin95@gmail.com>
2022-07-11 18:46:00 +0300

    🔥️ [BE] Db.Entities export entitiesToDto

--- db/models/utils.go
+++ db/models/utils.go
@@ -6,7 +6,7 @@ type entity[T any] interface {
 	Dto() T
 }
 
-func entitiesToDto[Dto any, Entity entity[Dto]](list []Entity) []Dto {
+func EntitiesToDto[Dto any, Entity entity[Dto]](list []Entity) []Dto {
 	return utils.Map(list, func(item Entity) Dto {
 		return item.Dto()
 	})
