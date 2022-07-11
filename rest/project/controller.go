b0dfd52cbe71ce85afc872fdf89bd5ed794cc604
Alexander <valverin95@gmail.com>
2022-07-11 17:22:03 +0300

    🔥️ [BE] Rest.Project: inviteUser

--- rest/project/controller.go
+++ rest/project/controller.go
@@ -23,6 +23,7 @@ func (c Controller) Init() {
 	c.update()
 	c.get()
 	c.getAll()
+	c.inviteUser()
 }
 
 func (c Controller) create() {
@@ -144,10 +145,62 @@ func (c Controller) update() {
 	})
 }
 
+func (c Controller) inviteUser() {
+	c.App.Post(path+"/:id/invite", func(ctx *fiber.Ctx) error {
+		type request struct {
+			UserID uint `json:"userId"`
+		}
+
+		user := auth.TakeUser(ctx)
+		projectID := ctx.Params("id", "-1")
+
+		var req request
+		if err := ctx.BodyParser(&req); err != nil {
+			err := handler.ErrorInvalidRequest
+			return ctx.Status(err.Status).JSON(err)
+		}
+
+		var project models.Project
+		err := db.DB.
+			Where("id=? AND user_id=?", projectID, user.ID).
+			First(&project).
+			Error
+
+		if err != nil {
+			err := handler.ErrorEntityNotFound
+			return ctx.Status(err.Status).JSON(err)
+		}
+
+		var invitedUser models.User
+		err = db.DB.
+			Where("id=?", req.UserID).
+			First(&invitedUser).
+			Error
+
+		if err != nil {
+			err := handler.Error{
+				Message: "invited user not found",
+				Status:  fiber.StatusNotFound,
+			}
+			return ctx.Status(err.Status).JSON(err)
+		}
+
+		prManager := getPrManager(project.ID)
+
+		prManager.InvitedUsers = append(prManager.InvitedUsers, invitedUser)
+		db.DB.Save(&prManager)
+
+		project.Manager = prManager
+		return ctx.JSON(project.Dto())
+	})
+}
+
 func getPrManager(projectID uint) models.ProjectManager {
 	prManager := models.ProjectManager{ProjectID: projectID}
 	db.DB.
 		Where(prManager).
+		Preload("InvitedUsers").
+		Preload("MemberedUsers").
 		FirstOrCreate(&prManager)
 
 	return prManager
